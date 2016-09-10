a3896be97479440481532c5967ee9cec459c761b
Andreas Hechenberger <poxy@greenstation.de>
2016-09-11 00:08:57 +0200

    modify staticfiles

--- staticfiles/admin/js/admin/RelatedObjectLookups.js
+++ staticfiles/admin/js/admin/RelatedObjectLookups.js
@@ -120,7 +120,7 @@
         var selects = django.jQuery(selectsSelector);
         selects.find('option').each(function() {
             if (this.value === objId) {
-                this.innerHTML = newRepr;
+                this.textContent = newRepr;
                 this.value = newId;
             }
         });
@@ -176,6 +176,14 @@
             }
         });
         $('.related-widget-wrapper select').trigger('change');
+        $('.related-lookup').click(function(e) {
+            e.preventDefault();
+            var event = $.Event('django:lookup-related');
+            $(this).trigger(event);
+            if (!event.isDefaultPrevented()) {
+                showRelatedObjectLookupPopup(this);
+            }
+        });
     });
 
 })(django.jQuery);
